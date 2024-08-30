746f0937c2d6b86cd30d7dd2ca0a191783a3804a
sierrashaw <sierra.shaw@menloschool.org>
2024-08-29 17:49:05 -0700

    Added comments and created a loop to convert from binary to decimal in #3.

--- src/Riddler.java
+++ src/Riddler.java
@@ -11,17 +11,22 @@ public class Riddler {
     public String decryptOne(String encrypted) {
         String decrypted = "";
         char shifted = ' ';
+        // Iterate through the letters in the encrypted string
         for (int i = 0; i < encrypted.length(); i++) {
+            // If the character is a letter, shift it by 9 -> the Caesar cipher
             if (Character.isAlphabetic(encrypted.charAt(i))) {
+                // When shifting, make sure that it doesn't spill over by using modulus 26
                 if (Character.isUpperCase(encrypted.charAt(i))) {
                     shifted = (char)((encrypted.charAt(i) - 'A' + 9) % 26 + 'A');
                 }
                 else {
                     shifted = (char)((encrypted.charAt(i) - 'a' + 9) % 26 + 'a');
                 }
+                // Add the character to the decrypted String
                 decrypted += shifted;
             }
             else {
+                // If the character isn't alphabetic, just add it
                 decrypted += encrypted.charAt(i);
             }
         }
@@ -33,9 +38,13 @@ public class Riddler {
         String decrypted = "";
         String mini = "";
         for (int i = 0; i < encrypted.length(); i++) {
+            // Each number is separated by spaces, so if there isn't a space at the given character,
+            // add it to a temp string called mini
             if (encrypted.charAt(i) != ' ') {
                 mini += encrypted.charAt(i);
             }
+            // Once you reach a space, convert the mini temp (representing the number) to an int of that String
+            // Then convert it to a char to add to decrypted and reset mini to an empty string for the next traverse
             else {
                 int num = Integer.parseInt(mini);
                 decrypted += (char) num;
@@ -47,17 +56,26 @@ public class Riddler {
     }
 
     public String decryptThree(String encrypted) {
-        // TODO: Complete the decryptThree() function.
         String decrypted = "";
         String binary = "";
+        int power = 1;
         for (int i = 0; i < encrypted.length() + 1; i++) {
+            // If it hasn't reached the 8 character length (8-bit binary), keep adding the characters (0 or 1) to the binary String
             if (i == 0 || i % 8 != 0) {
                 binary += encrypted.charAt(i);
             }
+            // Once the binary String is 8 characters, convert it to decimal and add the char version of the decimal to the decrypted string
+            // Then, reset the binary String for the next traverse
             else {
-                int parseInt = Integer.parseInt(binary, 2);
-                decrypted += (char)parseInt;
+                int shift = 0;
+                // Altering the multiplicative value so that the power increases for each value
+                for (int j = 0; j < binary.length(); j++) {
+                    shift += (binary.charAt(binary.length() - 1 - j) - '0') * power;
+                    power = power * 2;
+                }
+                decrypted += (char)shift;
                 binary = "";
+                power = 1;
             }
         }
         System.out.println(decrypted);
@@ -67,8 +85,10 @@ public class Riddler {
     public String decryptFour(String encrypted) {
         String decrypted = "";
         char shifted = ' ';
-        // TODO: Complete the decryptFour() function.
         for (int i = 0; i < encrypted.length(); i++) {
+            // codePointAt converts the symbol to decimal (using Unicode)
+            // Subtracting 9951 because that's the shift from the Unicode truck value (representing the space) and then added the ' ' value back
+            // to shift it back
             shifted = (char) ((encrypted.codePointAt(i) - 9951) + ' ');
             decrypted += shifted;
         }
